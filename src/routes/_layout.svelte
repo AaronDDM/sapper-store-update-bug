408328994a740833ea4d55273fac82e9d09b1a88
Aaron de Mello <aaron.d@nylas.com>
2021-03-23 13:11:42 -0400

    Bug PoC

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,35 @@
+<script>
+	import { stores } from "@sapper/app";
+	const { session, page } = stores();
+	import Nav from '../components/Nav.svelte';
+
+	page.subscribe((p) => {
+		console.debug("called")
+	})
+	setTimeout(() => {
+		session.update((s) => {
+			console.debug("Ran")
+			return {...s};
+		});
+	}, 5000);
+
+
+	export let segment;
+</script>
+
+<style>
+	main {
+		position: relative;
+		max-width: 56em;
+		background-color: white;
+		padding: 2em;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+</style>
+
+<Nav {segment}/>
+
+<main>
+	<slot></slot>
+</main>
---
+++ src/routes/index.svelte
@@ -0,0 +1,63 @@
+<script>
+	import { stores } from "@sapper/app";
+	import successkid from 'images/successkid.jpg';
+	const { session, page } = stores();
+
+	page.subscribe((p) => {
+		console.debug('[page.subscribe] called');
+	})
+
+	setTimeout(() => {
+		session.update((s) => {
+			console.debug('[session.update] called');
+			return {...s}
+		})
+	})
+</script>
+
+<style>
+	h1, figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+<h1>Great success!</h1>
+
+<figure>
+	<img alt="Success Kid" src="{successkid}">
+	<figcaption>Have fun with Sapper!</figcaption>
+</figure>
+
+<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
